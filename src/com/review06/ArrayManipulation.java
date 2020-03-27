7c58e981d8029a9364a2533ca192df52410bffcd
mrSerhan <mrserhan@hotmail.com>
2020-03-26 20:53:35 -0500

    Array Manipulation example

---
+++ src/com/review06/ArrayManipulation.java
@@ -0,0 +1,51 @@
+package com.review06;
+
+public class ArrayManipulation {
+
+	public int largestNumber(int[] array) {
+		
+		int largest = array[0];
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] > largest) {
+				largest = array[i];
+			}
+		}		
+		return largest;
+	}
+
+	int smallestNumber(int[] array) {
+		int smallest = array[0];
+		for (int num : array) {
+			if(num < smallest) {
+				smallest = num;		
+				}
+			}
+		return smallest;
+	}
+	
+	private static double findAverage(int[] array) {
+		
+		double sum = 0;
+		for (int num : array) {
+			sum+=num;
+		}
+		
+		return sum/array.length;
+	}
+	
+	public static void main (String[] args) {
+
+		int [] array = {5,2,9,6,1,8};
+		ArrayManipulation manip = new ArrayManipulation();
+		
+		int largest = manip.largestNumber(array);
+		System.out.println("Largest is "+largest);
+		System.out.println("Smallest is "+manip.smallestNumber(array));
+		
+		System.out.println(findAverage(array));
+		
+		//HW 
+		// create a method that returns sum of an array
+	}
+	
+}
